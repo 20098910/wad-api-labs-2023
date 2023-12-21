1acd6a3d36f8bdbf5ffc78ca88402b53a0dee5c2
ShaneWhitmore <shane.s.whitmore@gmail.com>
2023-12-21 20:35:11 +0000

    Mongo lab Exercises done

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
 import usersRouter from './api/users';
+import cors from 'cors';
 
 
 
@@ -28,6 +29,8 @@ app.use('/api/tasks', tasksRouter);
 
 app.use('/api/users', usersRouter);
 
+// Enable CORS for all requests
+app.use(cors());
 
 
 app.use(errHandler);
